65fb6e76e3cf7a01bfbf410e31873bb050027c4e
Anton Niklasson <niklasson.anton@gmail.com>
2012-04-29 16:13:51 +0200

    Cleaned up a bit

--- random-post-widget.php
+++ random-post-widget.php
@@ -9,18 +9,16 @@ Author URI: http://www.antonniklasson.se
 */
 
 
-// Make sure we don't expose any info if called directly
 if ( !function_exists( 'add_action' ) ) {
-	echo "Hi there!  I'm just a plugin, not much I can do when called directly.";
+	echo "Not allowed to load directly.";
 	exit;
 }
-define('RPW_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+
+
+
 require "random-post-widget-functions.php";
 
-
-
 class RandomPostWidget extends WP_Widget {
-	
 	private $post;
 	private $post_title;
 	private $post_url;
@@ -32,8 +30,8 @@ class RandomPostWidget extends WP_Widget {
 		$this->post_url = $post['permalink'];
 		
 		parent::__construct(
-	 		'Random_Post_Widget', // Base ID
-			'Random Post Widget', // Name
+	 		'Random_Post_Widget',
+			'Random Post Widget',
 			array('description' => __('Random Post Widget', 'text_domain'),)
 		);
 	}
@@ -44,31 +42,28 @@ class RandomPostWidget extends WP_Widget {
 		} else {
 			$title = __('New title', 'text_domain');
 		}
-		?>
-		<p>
-		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-		</p>
-		<?php 
+		
+		echo "<p>";
+		echo "<label for=".$this->get_field_id('title').">"._e( 'Title:' )."</label>"; 
+		echo "<input class='widefat' id=".$this->get_field_id('title')." name=".$this->get_field_name('title')." type='text' value=".esc_attr($title)." />";
+		echo "</p>";
 	}
 
 	public function update($new_instance, $old_instance) {
-		$instance = array(
+		return array(
 			'title' => strip_tags($new_instance['title'])
 		);
-
-		return $instance;
 	}
 
 	public function widget( $args, $instance ) {
-		extract( $args );
-		$title = apply_filters( 'widget_title', $instance['title']);
+		extract($args);
+		$title = apply_filters('widget_title', $instance['title']);
 
 		echo $before_widget;
-		if ( ! empty( $title ) )
+		if(!empty( $title))
 			echo $before_title . $title . $after_title;
 			echo "<a href='".$this->post_url."'>";
-				echo $this->post_title;
+			echo $this->post_title;
 			echo "</a>";
 		echo $after_widget;
 	}
@@ -78,7 +73,6 @@ class RandomPostWidget extends WP_Widget {
 function rwp_register_widget() {
 	register_widget('RandomPostWidget');
 }
-
 add_action('widgets_init', 'rwp_register_widget');
 
 ?>
